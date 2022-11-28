7529732c0e8b52a5ce5cc0493407494527541f07
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2022-11-28 09:01:16 -0300

    adicionando eureka server

--- hr-worker/src/main/java/com/example/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/example/hrworker/resources/WorkerResource.java
@@ -2,7 +2,11 @@ package com.example.hrworker.resources;
 
 import com.example.hrworker.entities.Worker;
 import com.example.hrworker.repositories.WorkerRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +15,11 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     private WorkerRepository repository;
@@ -23,6 +32,9 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        logger.info("Port = " + env.getProperty("local.server.port"));
+
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok().body(obj
         );
